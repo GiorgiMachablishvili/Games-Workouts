c747586ffacc5c0a3f56468fb651d636b8aa6f84
GiorgiMachablishvili <g.machablishvili@yahoo.com>
2025-02-27 19:18:44 +0400

    change display name, change url

--- Betus2/Extention/URL+Extension.swift
+++ Betus2/Extention/URL+Extension.swift
@@ -2,32 +2,32 @@
 
 extension String {
     static func userCreate() -> String {
-        let baseURL = "https://betus-workouts-98df47aa38c2.herokuapp.com/api/v1/users/"
+        let baseURL = "https://www.beat-sports.pro/api/v1/users/"
         return "\(baseURL)"
     }
 
     static func userDelete(userId: String) -> String {
-        let baseURL = "https://betus-workouts-98df47aa38c2.herokuapp.com/api/v1/users/"
+        let baseURL = "https://www.beat-sports.pro/api/v1/users/"
         return "\(baseURL)\(userId)"
     }
 
     static func createWorkoutCountsAndDate(userId: String) -> String {
-        let baseURL = "https://betus-workouts-98df47aa38c2.herokuapp.com/api/v1/workout_scores/?user_id="
+        let baseURL = "https://www.beat-sports.pro/api/v1/workout_scores/?user_id="
         return "\(baseURL)\(userId)"
     }
 
     static func getWorkoutCountsAndDate(userId: String) -> String {
-        let baseURL = "https://betus-workouts-98df47aa38c2.herokuapp.com/api/v1/workout_scores/"
+        let baseURL = "https://www.beat-sports.pro/api/v1/workout_scores/"
         return "\(baseURL)\(userId)"
     }
 
     static func postWorkoutDaysInARow(userId: String, date: String) -> String {
-        let baseURL = "https://betus-workouts-98df47aa38c2.herokuapp.com/api/v1/training_streaks/?user_id="
+        let baseURL = "https://www.beat-sports.pro/api/v1/training_streaks/?user_id="
         return "\(baseURL)\(userId)&training_date=\(date)"
     }
 
     static func getWorkoutDaysInARow(userId: String) -> String {
-        let baseURL = "https://betus-workouts-98df47aa38c2.herokuapp.com/api/v1/training_streaks/"
+        let baseURL = "https://www.beat-sports.pro/api/v1/training_streaks/"
         return "\(baseURL)\(userId)"
     }
 
